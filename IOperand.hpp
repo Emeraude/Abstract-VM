94b07c658bf0d767c54c152a240520fb5306ecba
Félix <felix.jacob@epitech.eu>
2015-02-16 19:02:10 +0100

    create class IOPerand and vm

---
+++ IOperand.hpp
@@ -0,0 +1,41 @@
+//
+// IOperand.hpp for AbstractVM in /home/jacob_f/gitlab/Abstract_VM
+// 
+// Made by felix jacob
+// Login   <jacob_f@epitech.net>
+// 
+// Started on  Mon Feb 16 18:47:10 2015 felix jacob
+// Last update Mon Feb 16 18:47:11 2015 felix jacob
+//
+
+#ifndef IOPERAND_HPP_
+# define IOPERAND_HPP_
+
+# include <string>
+
+enum eOperandType
+  {
+    INT8,
+    INT16,
+    INT32,
+    FLOAT,
+    DOUBLE,
+  };
+
+class	IOperand
+{
+public:
+  virtual std::string const	&toString() const = 0;
+
+  virtual int			getPrecision() const = 0;
+  virtual eOperandType		getType() const = 0;
+
+  virtual IOperand		*operator+(const IOperand &rhs) const = 0;
+  virtual IOperand		*operator-(const IOperand &rhs) const = 0;
+  virtual IOperand		*operator*(const IOperand &rhs) const = 0;
+  virtual IOperand		*operator/(const IOperand &rhs) const = 0;
+  virtual IOperand		*operator%(const IOperand &rhs) const = 0;
+
+};
+
+#endif /* !IOPERAND_HPP_ */
