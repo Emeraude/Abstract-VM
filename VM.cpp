0bc5139d394330f699f8647c23d7dd8e6c969594
broggi_t <broggi_t@epitech.eu>
2015-02-16 19:44:34 +0100

    starting to code VM class

---
+++ VM.cpp
@@ -0,0 +1,71 @@
+//
+// vm.cpp for  in /home/broggi_t/projet/Abstract_VM
+// 
+// Made by broggi_t
+// Login   <broggi_t@epitech.eu>
+// 
+// Started on  Mon Feb 16 19:04:52 2015 broggi_t
+// Last update Mon Feb 16 19:04:52 2015 broggi_t
+//
+
+#include <iostream>
+#include <sstream>
+#include "vm.hpp"
+
+VM::VM(const char *filename) {
+  std::ifstream file(filename);
+  std::stringstream buf;
+
+  if (filename) {
+    if (!file.good()) {
+      // throw
+    }
+    buf << file.rdbuf();
+    _buf = buf.str();
+    file.close();
+  }
+  else {
+    buf << std::cout.rdbuf();
+    _buf = buf.str();
+  }
+  _fptr["push"] = &VM::push;
+  _fptr["pop"] = &VM::pop;
+  _fptr["dump"] = &VM::dump;
+  _fptr["assert"] = &VM::assert;
+  _fptr["add"] = &VM::add;
+  _fptr["sub"] = &VM::sub;
+  _fptr["mul"] = &VM::mul;
+  _fptr["div"] = &VM::div;
+  _fptr["mod"] = &VM::mod;
+  _fptr["print"] = &VM::print;
+  _fptr["exit"] = &VM::exit;
+}
+
+VM::~VM() {}
+
+VM::VM(const VM& vm) {
+  _buf = vm._buf;
+  _stack = vm._stack;
+  _fptr = vm._fptr;
+}
+
+VM	&VM::operator=(const VM& vm) {
+  if (this != &vm) {
+    _buf = vm._buf;
+    _stack = vm._stack;
+    _fptr = vm._fptr;
+  }
+  return *this;
+}
+
+void	VM::push(std::string const &str) {}
+void	VM::pop(std::string const &str) {}
+void	VM::dump(std::string const &str) {}
+void	VM::assert(std::string const &str) {}
+void	VM::add(std::string const &str) {}
+void	VM::sub(std::string const &str) {}
+void	VM::mul(std::string const &str) {}
+void	VM::div(std::string const &str) {}
+void	VM::mod(std::string const &str) {}
+void	VM::print(std::string const &str) {}
+void	VM::exit(std::string const &str) {}
---
+++ VM.hpp
@@ -0,0 +1,47 @@
+//
+// vm.hpp for vm.hpp in /home/jacob_f/gitlab/Abstract_VM
+// 
+// Made by felix jacob
+// Login   <jacob_f@epitech.net>
+// 
+// Started on  Mon Feb 16 18:47:55 2015 felix jacob
+// Last update Mon Feb 16 19:01:27 2015 felix jacob
+//
+
+#ifndef VM_HPP_
+# define VM_HPP_
+
+#include <list>
+#include <map>
+#include <fstream>
+#include "IOperand.hpp"
+
+class	VM
+{
+private:
+
+  std::string							_buf;
+  std::list<IOperand *>						_stack;
+  std::map<std::string, void (VM::*)(std::string const &)>	_fptr;
+
+  void	push(std::string const &str);
+  void	pop(std::string const &str);
+  void	dump(std::string const &str);
+  void	assert(std::string const &str);
+  void	add(std::string const &str);
+  void	sub(std::string const &str);
+  void	mul(std::string const &str);
+  void	div(std::string const &str);
+  void	mod(std::string const &str);
+  void	print(std::string const &str);
+  void	exit(std::string const &str);
+
+public:
+
+  VM(const char * filename = NULL);
+  ~VM();
+  VM(const VM &);
+  VM	&operator=(const VM &);
+};
+
+#endif /* !VM_HPP_H */
